3fbd9ccb8bb569e9a2c4d3a2334a4831123d0559
Rich Colburn <the3dadvantage@gmail.com>
2019-05-29 07:38:27 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,36 +7,40 @@ bl_info = {
     "name": "Modeling Cloth",
     "author": "Rich Colburn, email: the3dadvantage@gmail.com",
     "version": (1, 0),
-    "blender": (2, 78, 0),
+    "blender": (2, 80, 0),
     "location": "View3D > Extended Tools > Modeling Cloth",
     "description": "Maintains the surface area of an object so it behaves like cloth",
-    "warning": "There might be an angry rhinoceros behind you",
+    "warning": "Your future self is planning to travel back in time to kill you",
     "wiki_url": "",
     "category": '3D View'}
 if "bpy" in locals():
     import imp
-    imp.reload(ModelingCloth)
-    imp.reload(SurfaceFollow)
-    imp.reload(UVShape)
-    imp.reload(DynamicTensionMap)
-    print("Reloaded multifiles")
+    imp.reload(ModelingCloth28)
+    #imp.reload(ModelingCloth)
+    #imp.reload(SurfaceFollow)
+    #imp.reload(UVShape)
+    #imp.reload(DynamicTensionMap)
+    print("Reloaded Modeling Cloth")
 else:
-    from . import ModelingCloth, SurfaceFollow, UVShape, DynamicTensionMap
-    print("Imported multifiles")
+    from . import ModelingCloth28#, SurfaceFollow, UVShape, DynamicTensionMap
+    #from . import ModelingCloth
+    print("Imported Modeling Cloth")
 
    
 def register():
-    ModelingCloth.register()
-    SurfaceFollow.register()
-    UVShape.register()
-    DynamicTensionMap.register()
+    ModelingCloth28.register()
+    #ModelingCloth.register()    
+    #SurfaceFollow.register()
+    #UVShape.register()
+    #DynamicTensionMap.register()
 
     
 def unregister():
-    ModelingCloth.unregister()
-    SurfaceFollow.unregister()
-    UVShape.unregister()
-    DynamicTensionMap.unregister()
+    ModelingCloth28.unregister()
+    #ModelingCloth.unregister()
+    #SurfaceFollow.unregister()
+    #UVShape.unregister()
+    #DynamicTensionMap.unregister()
 
     
 if __name__ == "__main__":
